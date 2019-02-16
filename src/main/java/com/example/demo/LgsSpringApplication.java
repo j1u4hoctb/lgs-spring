2eeb491b55e3e553b13590d036d07946f1960cd6
Oleg Leskiv <oleg.leskiv@godaddy.com>
2019-02-16 13:06:29 +0200

    Component Scope demonstration

--- src/main/java/com/example/demo/LgsSpringApplication.java
+++ src/main/java/com/example/demo/LgsSpringApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.component.LgsComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LgsSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LgsSpringApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(LgsSpringApplication.class, args);
+
+        LgsComponent beanByClass = applicationContext.getBean(LgsComponent.class);
+        LgsComponent beanByName = (LgsComponent) applicationContext.getBean("lgsComponent");
+
+        boolean sameReferenceCheck = beanByClass == beanByName;
+
+        System.out.println("Same reference check: " + sameReferenceCheck);
+
+        beanByClass.work();
+        beanByName.work();
     }
 
 }
---
+++ src/main/java/com/example/demo/component/LgsComponent.java
@@ -0,0 +1,24 @@
+package com.example.demo.component;
+
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class LgsComponent {
+
+    private Integer randomInteger;
+
+    @PostConstruct
+    public void init() {
+        Random random = new Random();
+        randomInteger = random.nextInt(100);
+    }
+
+    public void work() {
+        System.out.println("I'm a Logos component: " + randomInteger);
+    }
+
+}
